2c0a66e73bea807bafca1f83610e547c6d59dd64
milkinZ <ardhanurfajri157@gmail.com>
2023-03-29 05:46:26 +0700

    NextJS

--- pages/index.js
+++ pages/index.js
@@ -32,7 +32,7 @@ export default function Home() {
         </div>
       </div>
       <hr></hr>
-      <div className='text-center mt-12 space-y-60'>
+      <div className='text-center mt-12 p-4 space-y-60'>
         <div className='space-y-2'>
           <h1 className='text-4xl text-gray-200 font-mono font-semibold'>Welcome to My Page</h1>
           <p className='font-mono text-gray-100 text-sm'>Hi everybody, this is my first Next JS project</p>
